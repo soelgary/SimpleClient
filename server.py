9328fdc23917cb90321ddd359015b52d3d53f3e8
Gary Soeller <soeller.g@husky.neu.edu>
2014-09-12 13:35:36 -0400

    clean up

--- server.py
+++ server.py
@@ -5,18 +5,11 @@ CLASSNAME = 'cs5700fall2014'
 HOST = 'cs5700f14.ccs.neu.edu'
 
 def parse_request(data):
-  # need to parse the data
-  # then compute math if necessary
-  # then build a response message
   split_data = data.split(" ")
   bye = split_data[2]
-  print split_data
   if bye == 'BYE\n':
-    print 'THIS IS THE END'
     return {"end": True, "message": ""}
   operator = split_data[3]
-  print split_data
-  print operator
   number = None
   if operator == '+':
     number = int(split_data[2]) + int(split_data[4])
@@ -28,15 +21,12 @@ def parse_request(data):
     number = int(split_data[2]) / int(split_data[4])
   return {"end": False, "message": CLASSNAME + ' ' + str(number) + '\n'}
 
-#cs5700fall2014 HELLO [your NEU ID]\n
-
 def main(options, hostname, neu_id):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((HOST, options.port))
   s.sendall('cs5700fall2014 HELLO 000507111\n')
   while True:
     data = s.recv(256)
-    print 'Received', repr(data)
     parsed = parse_request(data)
     if parsed['end']:
       break
